2d0fca9f1680fdd5e8461658af68ea616ab8edb0
Bishal Bhattacharjee <103323091+Bishal097@users.noreply.github.com>
2024-07-04 23:44:21 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,80 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import sol from "./tech/sol.png";
+import eth from "./tech/eth.png";
+import cloud from "./tech/cloud.png";
+
+
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import h2s from "./company/h2s.png";
+import gdsc from "./company/gdsc.png";
+import aec from "./company/aec.png"
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+import amir from "./amir.jpg"
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  h2s,
+  gdsc,
+  cloud,
+  eth,
+  sol,
+  aec,
+  amir
+
+};
---
+++ src/constants/index.js
@@ -0,0 +1,240 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    html,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    h2s,
+    gdsc,
+    docker,
+    carrent,
+    jobit,
+    tripguide,
+    eth,
+    sol,
+    cloud,
+    aec,
+    amir
+  
+  } from "../assets";
+
+    
+
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Blockchain Developer",
+      icon: mobile,
+    },
+    {
+      title: "Competitive Programmer",
+      icon: backend,
+    },
+    {
+      title: "Cloud Enthusiast",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+   
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+  
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+ 
+    {
+      name: "git",
+      icon: git,
+    },
+   
+    {
+      name: "docker",
+      icon: docker,
+    },
+    {
+      name: "Etherium",
+      icon: eth,
+    },
+    {
+      name: "Solidity",
+      icon: sol,
+    },
+    {
+      name: "Cloud",
+      icon: cloud,
+    },
+
+  ];
+  
+  const experiences = [
+    {
+      title: "Campus Lead",
+      company_name: "Hack2Skill",
+      icon: h2s,
+      iconBg: "#E6DEDD",
+      date: " December 2022 - November 2023",
+      points: [
+        "Led a community of over 1000 members, organizing and executing various hackathons, coding competitions, and skill-building workshops. ",
+        "Collaborated with tech companies and startups to provide internship opportunities and real-world project experience.",
+        "Introduced peer coding sessions and coding challenges to promote hands-on learning and practical application of skills.",
+      ],
+    },
+    {
+      title: "CP Lead",
+      company_name: "GDSC",
+      icon: gdsc,
+      iconBg: "#E6DEDD",
+      date: "Aug 2023 -  Jul 2024",
+      points: [
+        "Led GDSC club's competitive programming initiatives, organizing coding events and workshops ",
+        "Increased club membership and engagement through innovative programming challenges.",
+        "Coordinated with industry experts for guest lectures and mentoring sessions.",
+        "Taught Data Structures and Algorithms to over 200 students, improving skills.",
+      ],
+    },
+    {
+      title: "Campus Lead",
+      company_name: "AEC CodingClub",
+      icon: aec,
+      iconBg: "#E6DEDD",
+      date: "Jan 2024 -  Present",
+      points: [
+        "Leading college coding club, organized successful events ",
+        "Mentored members in competitive programming techniques.",
+        "Facilitated peer learning and coding collaborations.",
+        "Enhanced members' coding skills and problem-solving abilities.",
+      ],
+    },
+   
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "Displays remarkable dedication and passion, delivering work of the highest quality on every project.",
+      name: "Sanamdeep Singh",
+      designation: "Mentor",
+      company: "Algozenith",
+      
+    },
+    {
+      testimonial:
+        "I've never met a developer who truly cares about his passion and dream  like Bishal does.",
+      name: "Md Amir Khan",
+      designation: "Student",
+      company: "AEC",
+      
+    },
+    {
+      testimonial:
+        "An outstanding team player who collaborates seamlessly, bringing both technical expertise and a positive energy.",
+      name: "Prayag Dwivedi",
+      designation: "Community Manager",
+      company: "Algozenith",
+      
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Rubik's Cube Solver",
+      description:
+        "Rubik's Cube Solver-using Korf's IDA* Algorithm. Achieved a solving time of under 3 seconds for a Rubik's cube jumbled 8 times using BFS, DFS and IDFS algorithms",
+      tags: [
+        {
+          name: "c++",
+          color: "blue-text-gradient",
+        },
+      ],
+      image: carrent,
+      
+    },
+    {
+      name: "BlogIt",
+      description:
+        "Developed a web based application which  serves as a full-text editor for writing content, including the ability to upload images for users",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Appwrite",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Redux",
+          color: "pink-text-gradient",
+        },
+      ],  
+      image: jobit,
+      
+    },
+    {
+      name: "White Board",
+      description:
+        "An interactive whiteboard built with React, offering refined functionalities and a seamless real-time collaborative experience for users.",
+      tags: [
+        {
+          name: "reactjs",
+          color: "green-text-gradient",
+        },
+       
+      ],
+      image: tripguide,
+      
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
